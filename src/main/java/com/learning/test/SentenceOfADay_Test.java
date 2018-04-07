4deb4cc692e4aed50486461933e7129621c71391
Iza <iza.wojcik.iw@gmail.com>
2018-04-07 10:42:56 +0200

    Test suit for SentenceOfADay has been added.

---
+++ src/main/java/com/learning/test/SentenceOfADay_Test.java
@@ -0,0 +1,48 @@
+package com.learning.test;
+
+import static org.junit.Assert.*;
+
+import java.util.Calendar;
+import java.util.concurrent.TimeUnit;
+
+import org.junit.Test;
+
+import com.learning.mockito_tutorial.Database;
+import com.learning.mockito_tutorial.SentenceOfADay;
+import com.learning.mockito_tutorial.Server;
+
+public class SentenceOfADay_Test {
+
+	private class DatabaseForTest extends Database{
+		@Override
+		public boolean has(String sentence) {
+			return true;
+		}
+	}
+	
+	private class ServerForTest extends Server{
+
+		public ServerForTest() {
+			super("http://address.com");
+		}
+		
+		@Override
+		public String getTodaysSentence()
+		{
+			return "Todays sentence";
+		}
+		
+	}
+	
+	DatabaseForTest databaseForTest = new DatabaseForTest();
+	ServerForTest serverForTest = new ServerForTest();
+	
+	SentenceOfADay sentenseOfADay = new SentenceOfADay(databaseForTest, serverForTest);
+	
+	@Test
+	public void getSentence_sentenceInDatabase_returnNothingNew() {
+		assertEquals(SentenceOfADay.sentenceIsInDatabase, sentenseOfADay.getSentence());
+		
+	}
+
+}
